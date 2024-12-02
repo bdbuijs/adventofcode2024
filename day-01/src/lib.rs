38c0673edf31790b4134f6ca3a8f07adadedf4e4
bdbuijs <bdbuijs@gmail.com>
2024-12-03 00:01:30 +0100

    Add examples to day-00 and day-01

--- day-01/src/lib.rs
+++ day-01/src/lib.rs
@@ -34,14 +34,14 @@ mod tests {
 
     #[test]
     fn part1() {
-        let input = "";
+        let input = fs::read_to_string("./part1-example1.txt").unwrap()
         let result = process_part1(input);
         assert_eq!(result, "");
     }
 
     #[test]
     fn part2() {
-        let input = "";
+        let input = fs::read_to_string("./part2-example1.txt").unwrap()
         let result = process_part2(input);
         assert_eq!(result, "");
     }
