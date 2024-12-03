a1ed044a7a4d1391943a0608c889b6f32fee0a9b
bdbuijs <bdbuijs@gmail.com>
2024-12-03 11:55:30 +0100

    refactor solution

--- day-01/src/lib.rs
+++ day-01/src/lib.rs
@@ -1,15 +1,8 @@
-use std::fs;
-
 use itertools::Itertools;
 
 use nom::{
-    branch::alt,
-    bytes::complete::tag,
-    character::complete::{
-        alpha1, char as nomchar, newline, one_of, space0, space1, u32 as nomu32,
-    },
-    combinator::recognize,
-    multi::{many1, separated_list1},
+    character::complete::{newline, space1, u32 as nomu32},
+    multi::separated_list1,
     sequence::tuple,
     IResult,
 };
@@ -21,25 +14,19 @@ pub fn process_part1(input: &str) -> String {
     second.sort();
     let total_difference = first
         .into_iter()
-        .zip(second.into_iter())
+        .zip(second)
         .map(|(f, s)| f.abs_diff(s))
         .sum::<u32>();
     total_difference.to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
-    let (input, (mut first, mut second)) = parse_input(input).unwrap();
+    let (input, (first, second)) = parse_input(input).unwrap();
     assert!(input.is_empty());
     let second = second.into_iter().counts();
     let total = first
         .into_iter()
-        .filter_map(|f| {
-            if let Some(s) = second.get(&f) {
-                Some(f * (*s as u32))
-            } else {
-                None
-            }
-        })
+        .filter_map(|f| second.get(&f).map(|s| f * (*s as u32)))
         .sum::<u32>();
     total.to_string()
 }
@@ -66,6 +53,7 @@ fn parse_line(input: &str) -> IResult<&str, Line> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
 
     #[test]
     fn part1() {
